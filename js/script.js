711a51e5a5bfea5928b2a633acfbd351106e76cf
Connor <csims9@hotmail.com>
2023-01-10 14:06:21 -0800

    Create router function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,49 @@
-"use strict";
+'use strict';
 
 const siteController = {
+    validFerryIDs: [],
+    validTerminalIDs: [],
+    htmlWriteTarget: document.querySelector('main#main'),
+    router: function() {
+        const capturedURL = window.location.href;
+        if (capturedURL === 'https://bcfarchive.ca/') {
+            createMainPage();
+            return;
+        };
+        if (capturedURL.includes('https://bcfarchive.ca/ferries/' || 'https://bcfarchive.ca/ferries')) {
+            let capturedFerryID = capturedURL.substring(30);
+            if (capturedFerryID.includes('/')) {
+                capturedFerryID = capturedFerryID.slice(0, -1);
+            };
+            if (capturedFerryID === '') {
+                createFerriesPage();
+                return;
+            };
+            if (this.validFerryIDs.includes(capturedFerryID)) {
+                createSingleFerryPage(capturedFerryID);
+                return;
+            } else {
+                create404Page();
+                return;
+            };
+        };
+        if (capturedURL.includes('https://bcfarchive.ca/terminal/')) {
+            let capturedTerminalID = capturedURL.substring(31);
+            if (capturedTerminalID.includes('/')) {
+                capturedTerminalID = capturedTerminalID.slice(0, -1);
+            };
+            if (capturedTerminalID === '') {
+                createTerminalsPage();
+                return;
+            };
+            if (this.validTerminalIDs.includes(capturedTerminalID)) {
+                createSingleTerminalPage(capturedTerminalID);
+                return;
+            } else {
+                create404Page();
+                return;
+            };
+        }
+    },
 
 };
