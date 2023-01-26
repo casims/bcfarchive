7503b472fb4ff1a05671d77bc328462e79daef76
Connor <csims9@hotmail.com>
2023-01-26 13:25:26 -0800

    Add functions for both single terminal & ferry pages

--- js/script.js
+++ js/script.js
@@ -67,6 +67,99 @@ const siteController = {
                     <img src="media/terminalss-thumbnail.jpg" alt="">
                 </a>
             </section>`;
+    },
+    createSingleFerryPage: function() {
+        this.htmlWriteTarget.innerHTML = '';
+        this.htmlBuffer = `
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <td>(ferryJSON.name)</td>
+                </tr>
+                <tr>
+                    <th>Picture</th>
+                    <td> <img src="(ferryJSON.picture)" alt=""></td>
+                </tr>
+                <tr>
+                    <th>Class</th>
+                    <td>(ferryJSON.class)</td>
+                </tr>
+                <tr>
+                    <th>Status</th>
+                    <td>(ferryJSON.status)</td>
+                </tr>
+                <tr>
+                    <th>Years Active</th>
+                    <td>(ferryJSON.years_active_start) - (ferryJSON.years_active_end</td>
+                </tr>
+                <tr>
+                    <th>Home Terminal</th>
+                    <td>(ferryJSON.home_terminal)</td>
+                </tr>
+                <tr>
+                    <th>Current Route</th>
+                    <td>(ferryJSON.current_route)</td>
+                </tr>
+                <tr>
+                    <th>Place of Origin</th>
+                    <td>(ferryJSON.origin)</td>
+                </tr>
+                <tr>
+                    <th>Engines</th>
+                    <td>(ferryJSON.engines)</td>
+                </tr>
+                <tr>
+                    <th>Horsepower</th>
+                    <td>(ferryJSON.horsepower)</td>
+                </tr>
+                <tr>
+                    <th>Max Speed</th>
+                    <td>(ferryJSON.max_speed)</td>
+                </tr>
+                <tr>
+                    <th>Length</th>
+                    <td>(ferryJSON.length)</td>
+                </tr>
+                <tr>
+                    <th>Displacement</th>
+                    <td>(ferryJSON.displacement)</td>
+                </tr>
+                <tr>
+                    <th>Vehicle Capacity</th>
+                    <td>(ferryJSON.vehicle_capacity)</td>
+                </tr>
+                <tr>
+                    <th>Passenger Capacity</th>
+                    <td>(ferryJSON.passenger_capacity)</td>
+                </tr>
+            </table>
+            <p>(ferryJSON.history)</p>`;
+    },
+    createSingleTerminalPage: function() {
+        this.htmlWriteTarget.innerHTML= '';
+        this.htmlBuffer = `
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <td>(terminalJSON.name)</td>
+                </tr>
+                <tr>
+                    <th>Picture</th>
+                    <td> <img src="(terminalJSON.picture)" alt=""></td>
+                </tr>
+                <tr>
+                    <th>Address</th>
+                    <td>(terminalJSON.class)</td>
+                </tr>
+                <tr>
+                    <th>Opened</th>
+                    <td>(terminalJSON.status)</td>
+                </tr>
+                <tr>
+                    <th>Routes</th>
+                    <td>(terminalJSON.status)</td>
+                </tr>
+            </table>
+            <p>(terminalJSON.history)</p>`;
     }
-
 };
