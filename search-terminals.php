97185b42acf22078067d3a41100f5815e5d8d2ef
Connor <csims9@hotmail.com>
2023-05-06 15:41:16 -0700

    Create search-terminals.php

---
+++ search-terminals.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+require("dbinfo.php");
+
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        
+if( mysqli_connect_errno() != 0 ){
+    die("<p>Could not connect to database, Error code:".$mysqli->connect_error."</p>");	
+}
+
+$recievedData = file_get_contents('php://input');
+
+if ($recievedData) {
+    $processedData = json_decode($recievedData);
+    $terminalsDataQuery = `SELECT * FROM terminals WHERE name LIKE '%$processedData%'`;
+    $capturedTerminalsData = $mysqli->query($terminalsDataQuery);
+    $processedTerminalsData = array();
+        while ($singleTerminal = mysqli_fetch_assoc($capturedTerminalsData)) {
+            $processedTerminalsData[] = $singleTerminal;
+        };
+};
+
+echo json_encode($processedTerminalsData);
+
+?>
